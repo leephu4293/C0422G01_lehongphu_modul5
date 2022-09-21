f797d5a5680809d93c76206e891013ee57aa4dce
leephu4293 <lehongphu4293@gmail.com>
2022-09-21 13:57:50 +0700

    up

---
+++ exercise/fibonaci/src/index.ts
@@ -0,0 +1,19 @@
+
+function fibonacci(num: number):number {
+
+    if (num ==1){
+        return 0;
+    } else if (num==2){
+        return 1;
+    }else {
+        return fibonacci(num-1)+ fibonacci(num-2)
+    }
+}
+
+
+
+let sum = 0;
+  for(let i=1; i<=10;i++){
+      sum+= fibonacci(i);
+      console.log(sum);
+  }
