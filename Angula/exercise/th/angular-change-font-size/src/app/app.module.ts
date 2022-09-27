2df73ccaa78a92ffdf6ddca3578fa8a963dca35a
leephu4293 <lehongphu4293@gmail.com>
2022-09-27 17:15:06 +0700

    up

--- Angula/exercise/th/angular-change-font-size/src/app/app.module.ts
+++ Angula/exercise/th/angular-change-font-size/src/app/app.module.ts
@@ -13,6 +13,8 @@ import { ArticleComponent } from './article/article.component';
 import { LikeComponent } from './like/like.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { RatingBarComponentComponent } from './rating-bar-component/rating-bar-component.component';
+import { CountdownTimerComponent } from './countdown-timer/countdown-timer.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +27,9 @@ import { FooterComponent } from './footer/footer.component';
     ArticleComponent,
     LikeComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    RatingBarComponentComponent,
+    CountdownTimerComponent
   ],
   imports: [
     BrowserModule,
